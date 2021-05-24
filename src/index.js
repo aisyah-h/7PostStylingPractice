3fa7f6e3067dc62910f3289302187b02373b1635
aisyah-h <83572065+aisyah-h@users.noreply.github.com>
2021-05-24 13:34:36 +0800

    My attempt

--- src/index.js
+++ src/index.js
@@ -5,3 +5,32 @@
 //Apply the "heading" style in the styles.css
 //Dynamically change the color of the h1 using inline css styles.
 //Morning = red, Afternoon = green, Night = blue.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const date = new Date();
+const currentTime = date.getHours();
+
+console.log(currentTime);
+
+//here onwards is my own code -- wrong and doesnt get intended result
+const greeting = function (currentTime) {
+  if (currentTime < 12) {
+    return "Morning"
+  }
+  else if (12 < currentTime < 18) {
+    return "Afternoon"
+  }
+  else if (18 < currentTime < 24) {
+    return "Evening"
+  }
+  else {
+    return "Day"
+  }
+};
+
+ReactDOM.render(
+  <h1 style={date} + {greeting}>Good</h1>,
+  document.getElementById("root")
+);
